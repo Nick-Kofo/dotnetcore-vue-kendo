5cb126a0d760b2266f24f2849f4dee2b2672766b
Nic-Kofo <nickkofo@gmail.com>
2018-12-24 12:55:18 +0200

    Add project files.

---
+++ WebApplication/wwwroot/js/vue.js
@@ -0,0 +1,20 @@
+﻿Vue.use(VueResource)
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         users: []
+//     },
+//     methods: {
+//         loadUsers: function() {
+//             this.$http.get("https://jsonplaceholder.typicode.com/todos").then(response => {
+//                 this.users = response.body;
+//             }, error => {
+//                 console.error(error);
+//             });
+//         }
+//     },
+//     created: function () {
+//         this.loadUsers();
+//     }
+// })
